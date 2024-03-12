e2464a45abf2f11d90aac0c62a98088c10467dee
Skystapper <97273031+Skystapper@users.noreply.github.com>
2024-03-12 21:15:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+let currSong = new Audio()
 async function getSongs(){
     let a = await fetch("http://127.0.0.1:5500/song/")
     let response = await a.text()
@@ -22,11 +21,35 @@ let songs = []
 
 getSongs()
 
+const playMusic = (track) => {
+    const songURL = "/song/" + track.innerText;
+
+    fetch(songURL)
+        .then(response => {
+            if (response.ok) {
+                currSong.src = songURL;
+                currSong.play();
+                
+            } else {
+                // Use innerHTML if the GET request is not successful
+                clearErrorMessage();
+                currSong.src = "/song/" + track.innerHTML;
+                currSong.play();
+                
+            }
+        })
+        .catch(error => {
+            // Use innerHTML in case of an error
+            currSong.src = "/song/" + track.innerHTML;
+            currSong.play();
+            
+        });
+};
 
 
 
 async function main() {
- 
+
 let songs = await getSongs()
 console.log(songs)  
 
@@ -55,15 +78,26 @@ for (const song of songs) {
 }
 
 
-var audio = new Audio(songs[0])
-audio.play()
+// var audio = new Audio(songs[0])
+// audio.play()
 
-audio.addEventListener("loadeddata", ()=>{
-    let duration = audio.duration
-    console.log(duration)
-} )
+// audio.addEventListener("loadeddata", ()=>{
+//     let duration = audio.duration
+//     console.log(duration)
+// } )
 
-  
+Array.from(document.querySelector(".songlist").getElementsByTagName("li")  ).forEach(e => {
+    e.addEventListener('click', element=>{
+    console.log(e.querySelector(".info").getElementsByTagName("div")[0].innerHTML)
+    console.log(encodeURI(e.querySelector(".info").getElementsByTagName("div")[0]))
+
+    
+        const filePath = e.querySelector(".info").getElementsByTagName("div")[0]
+        playMusic(filePath);
+        
+    
+    
+})});
 }    
    
 main()
